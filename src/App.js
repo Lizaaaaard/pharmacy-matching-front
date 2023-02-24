7e59848338c352f3705c258f730c40b998163dbe
liza.rachevskaya@mail.ru <liza.rachevskaya@mail.ru>
2023-02-24 20:05:55 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './styles/App.css';
+import MedcItem from "./components/MedcItem";
 
 function App() {
+    const [medc, setMedc] = useState([
+        {title: 'ot ponosa', body:'tabletki ogon'},
+        {title: 'ot ponosa 2', body:'tabletki ogon'},
+        {title: 'ot ponosa 3', body:'tabletki ogon'},
+        {title: 'ot ponosa 4', body:'tabletki ogon'}
+    ])
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {medc.map(med =>
+            <MedcItem medc={med}/>
+        )}
     </div>
   );
 }
---
+++ src/components/MedcItem.jsx
@@ -0,0 +1,20 @@
+﻿import React, {useState} from 'react';
+
+const MedcItem = (props) => {
+    
+    return (
+        <div className="medicine">
+            <div className="medcTitle"><strong>{props.medc.title}</strong></div>
+            <div className="medcInfo">
+                <div>
+                    {props.medc.body}
+                </div>
+                <div className="medcButton">
+                    <button>Add to cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MedcItem;
